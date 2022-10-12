74cbe971e3c90e352186aa388b880fea55a6e79a
Rishabh Mahajani <rmahajani31@gmail.com>
2022-10-12 09:46:36 +0530

    created training script

--- MT.py
+++ MT.py
@@ -3,17 +3,6 @@
 
 # ## Initialization Code
 
-# In[2]:
-
-
-gpu_info = get_ipython().getoutput('nvidia-smi')
-gpu_info = '\n'.join(gpu_info)
-if gpu_info.find('failed') >= 0:
-  print('Not connected to a GPU')
-else:
-  print(gpu_info)
-
-
 # In[3]:
 
 
